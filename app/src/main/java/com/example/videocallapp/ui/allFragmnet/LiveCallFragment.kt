4bdbe2e957aad103f633b55edcadf1f150261491
sumitrai <developer2607@gmail.com>
2023-08-29 13:45:05 +0530

    LiveVideo Call Fragment Added

--- app/src/main/java/com/example/videocallapp/ui/allFragmnet/LiveCallFragment.kt
+++ app/src/main/java/com/example/videocallapp/ui/allFragmnet/LiveCallFragment.kt
@@ -3,21 +3,32 @@ package com.example.videocallapp.ui.allFragmnet
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.SurfaceView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.videocallapp.R
+import androidx.fragment.app.Fragment
+import com.example.videocallapp.databinding.FragmentLiveCallBinding
+import com.example.videocallapp.model.agora.media.RtcTokenBuilder2
+import io.agora.rtc2.ChannelMediaOptions
+import io.agora.rtc2.Constants
+import io.agora.rtc2.IRtcEngineEventHandler
 import io.agora.rtc2.RtcEngine
+import io.agora.rtc2.RtcEngineConfig
+import io.agora.rtc2.video.VideoCanvas
 
 class LiveCallFragment : Fragment() {
 
+    private val appId = "5fb926599aeb4ba391c29247cc3b6f71"
+
     var appCertificate = "b5065fbfa5ed4d8aba0c25de974502b1"
     var expirationTimeInSeconds = 3600
-    private val channelName = "papayacoders"
+    private val channelName = "sumit"
+
+    private lateinit var binding: FragmentLiveCallBinding
 
     private var token: String? = null
     private val uid = 0
@@ -51,24 +62,180 @@ class LiveCallFragment : Fragment() {
                 } != PackageManager.PERMISSION_GRANTED)
     }
 
-//    fun showMessage(message: String?) {
-//        context.runOnUiThread {
-//            Toast.makeText(
-//                applicationContext,
-//                message,
-//                Toast.LENGTH_SHORT
-//            ).show()
-//        }
-//    }
+    fun showMessage(message: String?) {
+        activity?.runOnUiThread() {
+            Toast.makeText(
+                context,
+                message,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live_call, container, false)
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_live_call, container, false)
+
+        binding = FragmentLiveCallBinding.inflate(layoutInflater)
+        return binding.root
+
+
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tokenBuilder =
+            RtcTokenBuilder2()
+        val timestamp = (System.currentTimeMillis() / 1000 + expirationTimeInSeconds).toInt()
+
+        println("UID token")
+        val result = tokenBuilder.buildTokenWithUid(
+            appId, appCertificate,
+            channelName, uid, RtcTokenBuilder2.Role.ROLE_PUBLISHER, timestamp, timestamp
+        )
+        println(result)
+
+
+        token = result
+
+        if (!checkSelfPermission()) {
+            activity?.let {
+                ActivityCompat.requestPermissions(
+                    it,
+                    REQUESTED_PERMISSIONS,
+                    PERMISSION_REQ_ID
+                )
+            };
+        }
+        setupVideoSDKEngine();
+
+
+        binding.apply {
+            JoinButton.setOnClickListener {
+                joinChannel(binding.JoinButton)
+            }
+
+            LeaveButton.setOnClickListener {
+                leaveChannel(binding.LeaveButton)
+            }
+        }
+
+
+    }
+
+    private val mRtcEventHandler: IRtcEngineEventHandler = object : IRtcEngineEventHandler() {
+        override fun onUserJoined(uid: Int, elapsed: Int) {
+            showMessage("Remote user joined $uid")
+
+            // Set the remote video view
+            activity?.runOnUiThread { setupRemoteVideo(uid) }
+        }
+
+        override fun onJoinChannelSuccess(channel: String, uid: Int, elapsed: Int) {
+            isJoined = true
+            showMessage("Joined Channel $channel")
+        }
+
+        override fun onUserOffline(uid: Int, reason: Int) {
+            showMessage("Remote user offline $uid $reason")
+            activity?.runOnUiThread { remoteSurfaceView!!.visibility = View.GONE }
+        }
+    }
+
+    private fun setupRemoteVideo(uid: Int) {
+        remoteSurfaceView = SurfaceView(context)
+        remoteSurfaceView!!.setZOrderMediaOverlay(true)
+        binding.remoteVideoViewContainer.addView(remoteSurfaceView)
+        agoraEngine!!.setupRemoteVideo(
+            VideoCanvas(
+                remoteSurfaceView,
+                VideoCanvas.RENDER_MODE_FIT,
+                uid
+            )
+        )
+        remoteSurfaceView!!.visibility = View.VISIBLE
+    }
+
+    private fun setupLocalVideo() {
+        localSurfaceView = SurfaceView(context)
+        binding.localVideoViewContainer.addView(localSurfaceView)
+        agoraEngine!!.setupLocalVideo(
+            VideoCanvas(
+                localSurfaceView,
+                VideoCanvas.RENDER_MODE_HIDDEN,
+                0
+            )
+        )
+    }
+
+
+    fun leaveChannel(view: View) {
+        if (!isJoined) {
+            showMessage("Join a channel first")
+        } else {
+            agoraEngine!!.leaveChannel()
+            showMessage("You left the channel")
+            if (remoteSurfaceView != null) remoteSurfaceView!!.visibility = View.GONE
+            if (localSurfaceView != null) localSurfaceView!!.visibility = View.GONE
+            isJoined = false
+        }
+    }
+
+//    override fun onBackPressed() {
+//        super.onBackPressed()
+//        agoraEngine!!.leaveChannel()
+//        showMessage("You left the channel")
+//        if (remoteSurfaceView != null) remoteSurfaceView!!.visibility = View.GONE
+//        if (localSurfaceView != null) localSurfaceView!!.visibility = View.GONE
+//        isJoined = false
+//
+//    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        agoraEngine!!.stopPreview()
+        agoraEngine!!.leaveChannel()
+
+        Thread {
+            RtcEngine.destroy()
+            agoraEngine = null
+        }.start()
+    }
+
+    private fun setupVideoSDKEngine() {
+        try {
+            val config = RtcEngineConfig()
+            config.mContext = context
+            config.mAppId = appId
+            config.mEventHandler = mRtcEventHandler
+            agoraEngine = RtcEngine.create(config)
+            // By default, the video module is disabled, call enableVideo to enable it.
+            agoraEngine!!.enableVideo()
+        } catch (e: Exception) {
+            showMessage(e.toString())
+        }
+    }
+
+    fun joinChannel(view: View) {
+        if (checkSelfPermission()) {
+            val options = ChannelMediaOptions()
+
+            options.channelProfile = Constants.CHANNEL_PROFILE_COMMUNICATION
+            options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER
+            setupLocalVideo()
+            localSurfaceView!!.visibility = View.VISIBLE
+            agoraEngine!!.startPreview()
+            agoraEngine!!.joinChannel(token, channelName, uid, options)
+        } else {
+            Toast.makeText(context, "Permissions was not granted", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
 
 }
