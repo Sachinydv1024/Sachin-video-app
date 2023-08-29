6ff687ba7fc91f3e1001ad1aed601b1b5d199ae1
sumitrai <developer2607@gmail.com>
2023-08-29 17:09:51 +0530

    Login SignUp Activity Added

---
+++ app/src/main/java/com/example/videocallapp/ui/activity/LoginSignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.videocallapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.videocallapp.R
+
+class LoginSignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_signup)
+    }
+}
