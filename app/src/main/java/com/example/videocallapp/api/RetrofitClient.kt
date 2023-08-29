bf2dde4176c199f70f8afad833b20fd500bdf0ff
sumitrai <developer2607@gmail.com>
2023-08-29 13:53:49 +0530

    naming convention improvement

--- app/src/main/java/com/example/videocallapp/api/RetrofitClient.kt
+++ app/src/main/java/com/example/videocallapp/api/RetrofitClient.kt
@@ -5,7 +5,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private var BASE_URL = "https://raw.githubusercontent.com/13yadav/API/main/"
-
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create()).build()
