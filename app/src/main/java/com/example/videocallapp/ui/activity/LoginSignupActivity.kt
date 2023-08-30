3eb0bd30eec322766bde00437eae482f084fdc33
sumitrai <developer2607@gmail.com>
2023-08-30 13:22:01 +0530

    Login SignUp Activity Added

--- app/src/main/java/com/example/videocallapp/ui/activity/LoginSignupActivity.kt
+++ app/src/main/java/com/example/videocallapp/ui/activity/LoginSignupActivity.kt
@@ -1,12 +1,79 @@
 package com.example.videocallapp.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.videocallapp.R
+import com.example.videocallapp.databinding.ActivityLoginSignupBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginSignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginSignupBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_signup)
+        binding = ActivityLoginSignupBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_login_signup)
+        setContentView(binding.root)
+        //for day mode only
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        auth = FirebaseAuth.getInstance()
+
+        // Set up Google Sign In options
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.googleSignInButton.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+        }
+    }
+
+    // Handle the result of Google Sign In
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken)
+            } catch (e: ApiException) {
+                // Handle Google Sign In failure
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // User is signed in
+                    val user = auth.currentUser
+                    Log.d("TAG", "firebaseAuthWithGoogle: " + "here in line no 61")
+                    val intent = Intent(this, SplashScreen::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // Authentication failed
+                    Log.d("TAG", "firebaseAuthWithGoogle: " + "here in line no 61")
+                }
+            }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
     }
 }
